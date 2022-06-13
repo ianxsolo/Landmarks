23b6b23e24515da046e6e1a27a0d5ad58bc0b43a
Julian I. Kamil <julian.kamil@gmail.com>
2022-06-13 11:05:46 -0400

    Add user profile editor

--- Landmarks/Views/Categories/CategoryHome.swift
+++ Landmarks/Views/Categories/CategoryHome.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct CategoryHome: View {
     @EnvironmentObject var modelData: ModelData
+    @State private var showingProfile = false
     
     var body: some View {
         NavigationView {
@@ -22,6 +23,8 @@ struct CategoryHome: View {
             }
             .navigationTitle("Featured")
             .listStyle(PlainListStyle())
+            .toolbar { Button { showingProfile.toggle() } label: { Label("User Profile", systemImage: "person.crop.circle") }}
+            .sheet(isPresented: $showingProfile) { ProfileHost().environmentObject(modelData)}
         }
     }
 }
